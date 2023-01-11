88add071a8e249883f162d86a6fcc2155da56759
Andrew Kim <andrewk2417@gmail.com>
2023-01-11 15:54:44 -0600

    finished yourAge and conversion

--- conversion/src/conversion/conversion.java
+++ conversion/src/conversion/conversion.java
@@ -6,10 +6,16 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class conversion extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField feetTxt;
+	private JTextField yardTxt;
 
 	/**
 	 * Launch the application.
@@ -44,7 +50,37 @@ public class conversion extends JFrame {
 		contentPane.add(feetLabel);
 		
 		JLabel yardLabel = new JLabel("Yards: ");
-		yardLabel.setBounds(291, 43, 74, 31);
+		yardLabel.setBounds(267, 35, 74, 31);
 		contentPane.add(yardLabel);
+		
+		JButton feetButton = new JButton(">>>");
+		feetButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				double feet = Double.parseDouble(feetTxt.getText());
+				yardTxt.setText(feet/3 + "");
+			}
+		});
+		feetButton.setBounds(146, 82, 117, 29);
+		contentPane.add(feetButton);
+		
+		JButton yardButton = new JButton("<<<");
+		yardButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				double yard = Double.parseDouble(yardTxt.getText());
+				feetTxt.setText(yard*3 + "");
+			}
+		});
+		yardButton.setBounds(393, 82, 117, 29);
+		contentPane.add(yardButton);
+		
+		feetTxt = new JTextField();
+		feetTxt.setBounds(136, 37, 130, 26);
+		contentPane.add(feetTxt);
+		feetTxt.setColumns(10);
+		
+		yardTxt = new JTextField();
+		yardTxt.setColumns(10);
+		yardTxt.setBounds(393, 37, 130, 26);
+		contentPane.add(yardTxt);
 	}
 }
