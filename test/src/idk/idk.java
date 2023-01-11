bc46c2b21c9d5e6879c49d365fbfaa9c2b973fab
Andrew Kim <andrewk2417@gmail.com>
2023-01-10 22:21:07 -0600

    made new java file

---
+++ test/src/idk/idk.java
@@ -0,0 +1,9 @@
+package idk;
+
+public class idk {
+    public static void main(String[] args){
+        for (int i = 0; i < (int)(Math.random() * 10 + 1); i++ ){
+            System.out.println(i);
+        }
+    }
+}
