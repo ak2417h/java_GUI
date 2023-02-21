84df161b315fd13fbc8a9b9f5f78c72efdd2ba72
Kim <K0909471@students.katyisd.org>
2023-02-21 11:05:19 -0600

    tictactoe

--- tic tac toe/src/tictactoe.java
+++ tic tac toe/src/tictactoe.java
@@ -15,6 +15,8 @@ import java.awt.event.MouseEvent;
 public class tictactoe extends JFrame {
 
 	private JPanel contentPane;
+	
+	boolean p = true;
 
 	private String[][] arr = {
 	{null,null,null},
@@ -55,8 +57,15 @@ public class tictactoe extends JFrame {
 		l1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				Image img = new ImageIcon("images/o.png").getImage().getScaledInstance(l1.getWidth(), l1.getHeight(),Image.SCALE_SMOOTH);
-				l1.setIcon(new ImageIcon(img));				
+				if (p) {
+					Image img = new ImageIcon("images/o.png").getImage().getScaledInstance(l1.getWidth(), l1.getHeight(),Image.SCALE_SMOOTH);
+					l1.setIcon(new ImageIcon(img));						
+				}
+				else {
+					Image img = new ImageIcon("images/x.png").getImage().getScaledInstance(l1.getWidth(), l1.getHeight(),Image.SCALE_SMOOTH);
+					l1.setIcon(new ImageIcon(img));
+				}
+				p = !p;
 			}
 		});
 		l1.setBounds(10, 11, 167, 141);
@@ -64,41 +73,153 @@ public class tictactoe extends JFrame {
 		l1.setBorder(blackline);
 		
 		JLabel l2 = new JLabel("");
+		l2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (p) {
+					Image img = new ImageIcon("images/o.png").getImage().getScaledInstance(l1.getWidth(), l1.getHeight(),Image.SCALE_SMOOTH);
+					l2.setIcon(new ImageIcon(img));						
+				}
+				else {
+					Image img = new ImageIcon("images/x.png").getImage().getScaledInstance(l1.getWidth(), l1.getHeight(),Image.SCALE_SMOOTH);
+					l2.setIcon(new ImageIcon(img));
+				}
+				p = !p;
+			}
+		});
 		l2.setBounds(218, 11, 167, 141);
 		contentPane.add(l2);
 		l2.setBorder(blackline);
 		
 		JLabel l3 = new JLabel("");
+		l3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (p) {
+					Image img = new ImageIcon("images/o.png").getImage().getScaledInstance(l1.getWidth(), l1.getHeight(),Image.SCALE_SMOOTH);
+					l3.setIcon(new ImageIcon(img));						
+				}
+				else {
+					Image img = new ImageIcon("images/x.png").getImage().getScaledInstance(l1.getWidth(), l1.getHeight(),Image.SCALE_SMOOTH);
+					l3.setIcon(new ImageIcon(img));
+				}
+				p = !p;
+			}
+		});
 		l3.setBounds(424, 11, 167, 141);
 		contentPane.add(l3);
 		l3.setBorder(blackline);
 		
 		JLabel l5 = new JLabel("");
+		l5.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (p) {
+					Image img = new ImageIcon("images/o.png").getImage().getScaledInstance(l1.getWidth(), l1.getHeight(),Image.SCALE_SMOOTH);
+					l5.setIcon(new ImageIcon(img));						
+				}
+				else {
+					Image img = new ImageIcon("images/x.png").getImage().getScaledInstance(l1.getWidth(), l1.getHeight(),Image.SCALE_SMOOTH);
+					l5.setIcon(new ImageIcon(img));
+				}
+				p = !p;
+			}
+		});
 		l5.setBounds(218, 181, 167, 141);
 		contentPane.add(l5);
 		l5.setBorder(blackline);
 		
 		JLabel l8 = new JLabel("");
+		l8.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (p) {
+					Image img = new ImageIcon("images/o.png").getImage().getScaledInstance(l1.getWidth(), l1.getHeight(),Image.SCALE_SMOOTH);
+					l8.setIcon(new ImageIcon(img));						
+				}
+				else {
+					Image img = new ImageIcon("images/x.png").getImage().getScaledInstance(l1.getWidth(), l1.getHeight(),Image.SCALE_SMOOTH);
+					l8.setIcon(new ImageIcon(img));
+				}
+				p = !p;
+			}
+		});
 		l8.setBounds(218, 348, 167, 141);
 		contentPane.add(l8);
 		l8.setBorder(blackline);
 		
 		JLabel l4 = new JLabel("");
+		l4.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (p) {
+					Image img = new ImageIcon("images/o.png").getImage().getScaledInstance(l1.getWidth(), l1.getHeight(),Image.SCALE_SMOOTH);
+					l4.setIcon(new ImageIcon(img));						
+				}
+				else {
+					Image img = new ImageIcon("images/x.png").getImage().getScaledInstance(l1.getWidth(), l1.getHeight(),Image.SCALE_SMOOTH);
+					l4.setIcon(new ImageIcon(img));
+				}
+				p = !p;
+			}
+		});
 		l4.setBounds(10, 181, 167, 141);
 		contentPane.add(l4);
 		l4.setBorder(blackline);
 		
 		JLabel l6 = new JLabel("");
+		l6.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (p) {
+					Image img = new ImageIcon("images/o.png").getImage().getScaledInstance(l1.getWidth(), l1.getHeight(),Image.SCALE_SMOOTH);
+					l6.setIcon(new ImageIcon(img));						
+				}
+				else {
+					Image img = new ImageIcon("images/x.png").getImage().getScaledInstance(l1.getWidth(), l1.getHeight(),Image.SCALE_SMOOTH);
+					l6.setIcon(new ImageIcon(img));
+				}
+				p = !p;
+			}
+		});
 		l6.setBounds(424, 181, 167, 141);
 		contentPane.add(l6);
 		l6.setBorder(blackline);
 		
 		JLabel l9 = new JLabel("");
+		l9.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (p) {
+					Image img = new ImageIcon("images/o.png").getImage().getScaledInstance(l1.getWidth(), l1.getHeight(),Image.SCALE_SMOOTH);
+					l9.setIcon(new ImageIcon(img));						
+				}
+				else {
+					Image img = new ImageIcon("images/x.png").getImage().getScaledInstance(l1.getWidth(), l1.getHeight(),Image.SCALE_SMOOTH);
+					l9.setIcon(new ImageIcon(img));
+				}
+				p = !p;
+			}
+		});
 		l9.setBounds(424, 348, 167, 141);
 		contentPane.add(l9);
 		l9.setBorder(blackline);
 		
 		JLabel l7 = new JLabel("");
+		l7.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (p) {
+					Image img = new ImageIcon("images/o.png").getImage().getScaledInstance(l1.getWidth(), l1.getHeight(),Image.SCALE_SMOOTH);
+					l7.setIcon(new ImageIcon(img));						
+				}
+				else {
+					Image img = new ImageIcon("images/x.png").getImage().getScaledInstance(l1.getWidth(), l1.getHeight(),Image.SCALE_SMOOTH);
+					l7.setIcon(new ImageIcon(img));
+				}
+				p = !p;
+			}
+		});
 		l7.setBounds(10, 348, 167, 141);
 		contentPane.add(l7);
 		l7.setBorder(blackline);
