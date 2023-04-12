aa389df849b3f73fb4af54f580fbb1806c543617
Kim <K0909471@students.katyisd.org>
2023-04-12 11:08:27 -0500

    .

--- employee database/src/employeedatabase.java
+++ employee database/src/employeedatabase.java
@@ -149,9 +149,9 @@ public class employeedatabase extends JFrame {
 				try {
 					Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
 					con = DriverManager.getConnection("jdbc:ucanaccess://C://Users//k0909471//Downloads//compsci//employee database//employeedatabase.accdb");
-					JOptionPane.showMessageDialog(null, "Connection successful!!");
+//					JOptionPane.showMessageDialog(null, "Connection successful!!");
 					st = con.createStatement();
-					String sql = "select employeeID,FirstName,LastName,Salary,Department from student";
+					String sql = "select employeeID,FirstName,LastName,Salary,Department from employeedatabase";
 					rs = st.executeQuery(sql);
 					table.setModel(DbUtils.resultSetToTableModel(rs));
 				} catch (ClassNotFoundException | SQLException e1) {
@@ -164,14 +164,77 @@ public class employeedatabase extends JFrame {
 		contentPane.add(loadbtn);
 		
 		JButton btnAddEmployee = new JButton("Add Employee");
+		btnAddEmployee.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String id = eitxt.getText();
+				String first = fntxt.getText();
+				String last = lntxt.getText();
+				int s = Integer.parseInt(stxt.getText());
+				String d = dtxt.getText();
+				model = (DefaultTableModel) table.getModel();
+				String[] row = {id,first,last,""+s,d};
+				model.addRow(row);
+				try {
+					Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+					con = DriverManager.getConnection("jdbc:ucanaccess://C://Users//k0909471//Downloads//compsci//employee database//employeedatabase.accdb");
+					st = con.createStatement();
+					st.executeUpdate("insert into employeedatabase (employeeID,FirstName,LastName,Salary,Department) values('"+id+"','"+first+"','"+last+"','"+s+"','"+d+"')");
+				} catch (ClassNotFoundException | SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
 		btnAddEmployee.setBounds(10, 371, 105, 23);
 		contentPane.add(btnAddEmployee);
 		
 		JButton btnRemoveEmployee = new JButton("Remove Employee");
+		btnRemoveEmployee.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int deletedRow = table.getSelectedRow();
+				String cell = (String) table.getValueAt(deletedRow, 0);
+				model = (DefaultTableModel) table.getModel();
+				model.removeRow(deletedRow);
+				try {
+					Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+					con = DriverManager.getConnection("jdbc:ucanaccess://C://Users//k0909471//Downloads//compsci//employee database//employeedatabase.accdb");
+					st = con.createStatement();
+					st.executeUpdate("delete from employeedatabase where employeeID = '" + cell + "'");
+				} catch (ClassNotFoundException | SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
 		btnRemoveEmployee.setBounds(152, 371, 121, 23);
 		contentPane.add(btnRemoveEmployee);
 		
 		JButton btnUpdateEmployee = new JButton("Update Employee");
+		btnUpdateEmployee.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int updatedRow = table.getSelectedRow();
+				String id = eitxt.getText();
+				String first = fntxt.getText();
+				String last = lntxt.getText();
+				int s = Integer.parseInt(stxt.getText());
+				String d = dtxt.getText();
+				table.setValueAt(id, updatedRow, 0);
+				table.setValueAt(first, updatedRow, 1);
+				table.setValueAt(last, updatedRow, 2);
+				table.setValueAt(s, updatedRow, 3);
+				table.setValueAt(d, updatedRow, 4);
+				String cell = (String) table.getValueAt(updatedRow, 0);
+				try {
+					Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+					con = DriverManager.getConnection("jdbc:ucanaccess://C://Users//k0909471//Downloads//compsci//employee database//employeedatabase.accdb");
+					pst = con.prepareStatement("update employeedatabase set employeeID = '" + id + "',FirstName='" + first + "',LastName='" + last + "',Salary='" + s + "',Department='" + d + "' where employeeID = '" + cell + "'");
+					pst.executeUpdate();
+				} catch (ClassNotFoundException | SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
 		btnUpdateEmployee.setBounds(303, 371, 121, 23);
 		contentPane.add(btnUpdateEmployee);
 	}
